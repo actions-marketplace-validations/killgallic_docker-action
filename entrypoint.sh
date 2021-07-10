062782c6ef5231f848fe66a9f2e5b77f70a2217c
its <mystline7@gmail.com>
2021-07-10 14:23:59 +0930

    better secret handling and such

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ PASSWORD=$2
 REPOSITORY=$3
 REGISTRY=$4
 TAG=$5
+PAT=$6
+PAT_STRING=$7
 
 #if [ -z $USERNAME ]; then
 #  echo 'Required username parameter'
@@ -28,12 +30,24 @@ if [[ -z $TAG ]]; then
   TAG=$(date '+%Y%m%d%H%M%S')
 fi
 
+if [ -z $PAT ]; then
+  echo 'Required paramater PAT not passed'
+  exit 1
+fi
+
+if [ -z $PAT_STRING ]; then
+  echo 'Required paramater PAT_STRING not passed'
+  exit 1
+fi
+
+
 
 IMAGE=$REPOSITORY:$TAG
 if [ -n "$REGISTRY" ]; then
   IMAGE=$REGISTRY/$IMAGE
 fi
 
+git config --global credential.helper store && echo "$PAT_STRING" > ~/.git-credentials
 docker build -t $IMAGE .
 #docker login --username "$USERNAME" --password "$PASSWORD" $REGISTRY
 #docker push $IMAGE
